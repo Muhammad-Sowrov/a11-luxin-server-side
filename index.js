868d160cee2fa9a63083a2ce105cd64632b0aaec
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-11-08 11:14:02 +0600

    connect with mongoDB with secret user & password

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 const app = express();
@@ -12,6 +13,40 @@ app.use(express());
 // mongoDB
 
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.xz3kocr.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // mongoDB ends
 
